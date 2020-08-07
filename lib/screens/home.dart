e441b9c927ecfebc24baf437e93e995407a5891a
hchrist2010 <christhu@oregonstate.edu>
2020-08-06 22:20:30 -0700

    added the camera floating action button to take user to new_post page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -18,38 +18,45 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('${title} - ${amount}'),
-          centerTitle: true,
-        ),
-        body: StreamBuilder(
-            stream: Firestore.instance.collection('posts').snapshots(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                    itemCount: snapshot.data.documents.length,
-                    itemBuilder: (context, index) {
-                      var post = snapshot.data.documents[index];
-                      //setAmount(post['amount']);
-                      return ListTile(
-                        leading: Text(DateFormat.yMMMMEEEEd()
-                            .format(DateTime.parse(post['date']))),
-                        title: Text(post['amount'].toString()),
-                        onTap: () {
-                          Navigator.of(context).pushNamed(PostDetails.routName,
-                              arguments: WasteagramPost(
-                                url: post['url'],
-                                amount: post['amount'],
-                                latitude: post['latitude'],
-                                longitude: post['longitude'],
-                                date: DateTime.parse(post['date']),
-                              ));
-                        },
-                      );
-                    });
-              } else {
-                return Container(child: CircularProgressIndicator());
-              }
-            }));
+      appBar: AppBar(
+        title: Text('${title} - ${amount}'),
+        centerTitle: true,
+      ),
+      body: StreamBuilder(
+          stream: Firestore.instance.collection('posts').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) {
+                    var post = snapshot.data.documents[index];
+                    //setAmount(post['amount']);
+                    return ListTile(
+                      leading: Text(DateFormat.yMMMMEEEEd()
+                          .format(DateTime.parse(post['date']))),
+                      title: Text(post['amount'].toString()),
+                      onTap: () {
+                        Navigator.of(context).pushNamed(PostDetails.routName,
+                            arguments: WasteagramPost(
+                              url: post['url'],
+                              amount: post['amount'],
+                              latitude: post['latitude'],
+                              longitude: post['longitude'],
+                              date: DateTime.parse(post['date']),
+                            ));
+                      },
+                    );
+                  });
+            } else {
+              return Container(child: CircularProgressIndicator());
+            }
+          }),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        onPressed: () {
+        Navigator.of(context).pushNamed(NewPost.routName);
+      }),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
